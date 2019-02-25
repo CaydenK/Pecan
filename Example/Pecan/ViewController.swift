a82cd96a12ee57b1bd785ef7f0a660795ed9ca6b
CaydenK <caydenk@163.com>
2019-02-26 00:36:59 +0800

    测试demo，并测试类名重复情况

--- Example/Pecan/ViewController.swift
+++ Example/Pecan/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import Pecan
+import WebKit
+import TestModule
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKUIDelegate {
+    lazy var webview: Pecan.WebView = {
+        let webConfiguration = WKWebViewConfiguration()
+        let _webview = Pecan.WebView(frame: self.view.bounds, configuration: webConfiguration)
+        _webview.uiDelegate = self
+        return _webview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(self.webview)
+        self.webview.load(URLRequest(url: URL(string: "https://www.baidu.com")!))
+        
+        print(self.webview.uiDelegate as Any)
     }
 
     override func didReceiveMemoryWarning() {
