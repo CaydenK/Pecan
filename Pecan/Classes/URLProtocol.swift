0bede2b49dcff15c47fd136c5713d005e71e4f34
CaydenK <caydenk@163.com>
2019-02-26 14:27:24 +0800

    添加URLProtocol

---
+++ Pecan/Classes/URLProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  URLProtocol.swift
+//  Pods
+//
+//  Created by CaydenK on 2019/2/26.
+//
+
+import Foundation
+
+
+class WebURLProtocol: URLProtocol {
+}
+
+
+extension PecanExtension where Extend : URLProtocol {
+}
