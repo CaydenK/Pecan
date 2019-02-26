1dd5beea48a39aa5e99186b0628f498f36841a90
CaydenK <caydenk@163.com>
2019-02-26 18:13:48 +0800

    URLProtocol拦截

--- Pecan/Classes/URLProtocol.swift
+++ Pecan/Classes/URLProtocol.swift
@@ -8,8 +8,64 @@
 import Foundation
 import WebKit
 
+private let kWebURLProtocolKey = "kWebURLProtocolKey"
 
-class WebURLProtocol: URLProtocol {
+class WebURLProtocol: URLProtocol,URLSessionDelegate, URLSessionDataDelegate {
+    
+    private var webTask: URLSessionTask?
+    private var session: URLSession?
+    private var data: Data?
+    private var webThread: Thread?
+
+    
+    open override class func canInit(with request: URLRequest) -> Bool{
+        
+        return (URLProtocol.property(forKey: kWebURLProtocolKey, in: request) == nil);
+    }
+    
+    open override class func canonicalRequest(for request: URLRequest) -> URLRequest{
+        return request;
+    }
+    
+    open override func startLoading(){
+        let request = self.request;
+        print(request)
+        
+        guard (self.request.url?.lastPathComponent) != nil else{
+            return
+        }
+        
+        URLProtocol.setProperty(true, forKey: kWebURLProtocolKey, in: request as! NSMutableURLRequest)
+        let session = URLSession.init(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
+        self.webTask = session.dataTask(with: request)
+        self.task?.resume()
+    }
+    
+    open override func stopLoading() {
+        if self.webTask != nil{
+            self.webTask?.cancel()
+        }
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .allowed)
+        completionHandler(.allow)
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        self.client?.urlProtocol(self, didLoad: data)
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, willCacheResponse proposedResponse: CachedURLResponse, completionHandler: @escaping (CachedURLResponse?) -> Void) {
+        print(proposedResponse)
+        
+        completionHandler(proposedResponse);
+    }
+
 }
 
 
@@ -37,3 +93,4 @@ extension PecanExtension where Extend : URLProtocol {
         Thread.detachNewThreadSelector(Context.browser.unregister, toTarget: cls, with: scheme)
     }
 }
+
