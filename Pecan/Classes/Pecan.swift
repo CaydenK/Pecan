27f7aac7d70d5abba2fd2d816344810a4e128a1b
CaydenK <caydenk@163.com>
2019-02-28 23:07:53 +0800

    是否hook请求改为独立方法

--- Pecan/Classes/Pecan.swift
+++ Pecan/Classes/Pecan.swift
@@ -15,9 +15,18 @@ open class PecanEngine {
         UIWebView().loadHTMLString("", baseURL: nil) //start WebThread
         WKWebView().loadHTMLString("", baseURL: nil) //start thread named 'JavaScriptCore bmalloc scavenger'
         
-//        URLProtocol.registerClass(WebURLProtocol.self)
-//        URLProtocol.pecan.registerScheme(scheme: "https")
-//        URLProtocol.pecan.registerScheme(scheme: "http")
+    }
+    
+    public static func hookWebRequest() {
+        URLProtocol.registerClass(WebURLProtocol.self)
+        URLProtocol.pecan.registerScheme(scheme: "https")
+        URLProtocol.pecan.registerScheme(scheme: "http")
+    }
+    
+    public static func unhookWebRequest() {
+        URLProtocol.unregisterClass(WebURLProtocol.self)
+        URLProtocol.pecan.unregisterScheme(scheme: "https")
+        URLProtocol.pecan.unregisterScheme(scheme: "http")
     }
     
     public static func test2333() {
